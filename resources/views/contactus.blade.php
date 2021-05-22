b0b79f5ce66ca7002bd1a2988e1dfeaaea940a5c
iamhaydaryahya <haydarmadridista7@gmail.com>
2021-05-22 12:12:45 +0700

    Default

---
+++ resources/views/contactus.blade.php
@@ -0,0 +1,21 @@
+@extends('layout/main')
+
+@section('title', 'Contact US')
+
+@section('container')
+  <div class="container">
+      <div class="row">
+          <div class="col-10">
+              <h1 class="mt-3">Contact US</h1>
+              <img src="contact.jpg"/>
+              <h2> Identitas Diri</h2>
+              <p>Nama   : Haydar Yahya</p>
+              <p>NBI    : 1461900123</P>
+              <P>Kelas  : P</p>
+              <p>Contact: 082257481036</p>
+              <p>Email  : haydarmadridista7@gmail.com</p>
+              <p>IG     : iamhaydaryahya</p>
+          </div>
+        </div>
+    </div>
+  @endsection
